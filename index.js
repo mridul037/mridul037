0b0707a60577465d47d028a3299a059714fb6e96
Quarantine Coder <mridulshukla037@gmail.com>
2020-08-01 01:32:16 +0530

    code push

---
+++ index.js
@@ -0,0 +1,53 @@
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+
+const { promises: fs } = require("fs");
+
+const app = express();
+
+const Key = process.env.client_Id;
+console.log("please wait");
+
+async function main(){
+app.get("/", async (req, res) => {
+  const { data } = req.query;
+  if (!data) {
+    return res.status(401).send("Not authorized");
+  }
+  const readmeTemplate = (
+    await fs.readFile(path.join(process.cwd(), "./README.template.md"))
+  ).toString("utf-8");
+
+  const { en: qoth, author: qoth_author } = await (
+    await fetch("https://programming-quotes-api.herokuapp.com/quotes/random")
+  ).json();
+
+  const {data} = await fetch(
+    `http://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=${Key}`
+  ).json();
+  
+  const {total:val1}=data.article[0].title;
+  const {total:val2}=data.article[1].title;
+  const {total:val3}=data.article[2].title;
+  const {total:val4}=data.article[3].title;
+  const {total:val5}=data.article[4].title;
+  
+
+
+  console.log(data);
+  return res.send("data has come");
+});
+
+const readme=readmeTemplate
+    .replace("{qoth}", qoth)
+    .replace("{qoth_author}", qoth_author)
+    .replace("{val1}",val1)
+    .replace("{val2}",val2)
+    .replace("{val3}",val3)
+    .replace("{val4}",val4)
+    .replace("{val5}",val5)
+
+    await fs.writeFile("README.md", readme);
+}
+main()
