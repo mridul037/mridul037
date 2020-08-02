d22d78e17a08f2a1bfc23d6cc0c2e03d47923637
mridul037 <30683771+mridul037@users.noreply.github.com>
2020-08-02 16:26:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,32 +25,23 @@ const readmeTemplate = (
 
   
   
-  const getTitleDesc = async () => {
-    const { articles } = await fetch("http://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=077fe507985744818b3f405349c79601").then((res) => res.json());
-  
-  return articles;
-};
  
-(async ()=> {
+    const { articles } =await fetch("http://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=077fe507985744818b3f405349c79601").then((res) => res.json());
+  
+  
+
+ 
+
   const val=[]
-  const e = await getTitleDesc()
-  e.forEach(element => {
+  const des=[]
+  
+  articles.forEach(element => {
     val.push(element.title);
-    des.push(element.description)
+    des.push(element.description);
   })
-  readme=readmeTemplate
-  .replace("{val1}",val[0])
-  .replace("{val2}",val[1])
-  .replace("{val3}",val[2])
-  .replace("{val4}",val[3])
-  .replace("{val5}",val[4])
-  .replace("{des1}",des[0])
-  .replace("{des2}",des[1])
-  .replace("{des3}",des[2])
-  .replace("{des4}",des[3])
-  .replace("{des5}",des[4])
-    await fs.writeFile("README.md", readme);
-})()
+  
+   
+
 
 
 
@@ -59,6 +50,16 @@ const readmeTemplate = (
 const readme=readmeTemplate
     .replace("{qoth}", qoth)
     .replace("{qoth_author}", qoth_author)
+    .replace("{val1}",val[0])
+    .replace("{val2}",val[1])
+    .replace("{val3}",val[2])
+    .replace("{val4}",val[3])
+    .replace("{val5}",val[4])
+    .replace("{des1}",des[0])
+    .replace("{des2}",des[1])
+    .replace("{des3}",des[2])
+    .replace("{des4}",des[3])
+    .replace("{des5}",des[4])
    
     
 
